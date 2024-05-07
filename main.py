e3a50a6b0504007b83fe089e830902f19573ebdd
randradaj <rjandrada@twc.com>
2024-05-07 15:48:42 -0700

    add tokenizing, stopping, stemming, and character n-grams for user search query. placeholder will be updated once indexing/ranking documents is complete

---
+++ main.py
@@ -0,0 +1,37 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+class LemmaTokenizer:
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+
+    def __call__(self, doc):
+        return [self.lemmatizer.lemmatize(t) for t in word_tokenize(doc)]
+
+
+user_query = input("Enter your search query: ").lower()
+
+# Remove stopwords
+stop_words = set(stopwords.words('english'))
+
+tokens = word_tokenize(user_query)
+filtered_tokens = [token for token in tokens if token not in stop_words]
+filtered_query = ' '.join(filtered_tokens)
+
+# Lemmatize
+lemma_vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), token_pattern=None)
+lemma_token_vector = lemma_vectorizer.fit_transform([filtered_query])
+
+# converts lemmatized query into acceptable format for character n-grams
+lemmatized_tokens = lemma_vectorizer.get_feature_names_out()
+
+# Character n-grams
+ngram_vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(5, 5))
+ngram_token_vector = ngram_vectorizer.fit_transform(lemmatized_tokens)
+
+# Placeholder for retrieving and displaying ranked documents
+print("Vocabulary:", ngram_vectorizer.vocabulary_)
+print("Encoded vector shape:", ngram_token_vector.shape)
+print("Encoded vector array:", ngram_token_vector.toarray())
